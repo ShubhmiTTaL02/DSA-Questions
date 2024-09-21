06475a6eabd7d0afc5c102e688a50cd15991ed7e
ShubhmiTTaL02 <shubhmittalagra@gmail.com>
2024-09-21 16:04:42 +0530

    third problem

---
+++ LeetCode386.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> result=new ArrayList<>();
+        for(int i=1;i<=9;i++){
+            solve(i,n,result);
+        }
+        return result;
+    }
+    public void solve(int currentNum,int n,List<Integer> result){
+        if(currentNum>n){
+            return;
+        }
+        result.add(currentNum);
+        for(int j=0;j<=9;j++){
+            int newNum=currentNum*10+j;
+            if(newNum>n){
+                return;
+            }
+            solve(newNum,n,result);
+        }
+    }
+}
